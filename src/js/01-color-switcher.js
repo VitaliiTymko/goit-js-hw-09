d1bf14ca8b43da6f3fd00a8a8e5dad41ad251b50
VitaliiTymko <intehmet2015@gmail.com>
2022-11-04 22:10:20 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let intervalId = null;
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  console.log('color ${getRandomHexColor}');
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+  clearInterval(intervalId);
+});
