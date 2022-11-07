5d80a6998bb5ab119073cf40b507fe20bcc37b14
VitaliiTymko <intehmet2015@gmail.com>
2022-11-07 19:02:47 +0200

    finish task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,34 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+
+let selectedTime = null;
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = pad(Math.floor(ms / day));
+  const hours = pad(Math.floor((ms % day) / hour));
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+  return { days, hours, minutes, seconds };
+}
+
+refs.startBtn.setAttribute('disabled', true);
 
 const options = {
   enableTime: true,
@@ -9,50 +37,56 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
-    // diferentTime = futureDate - currentDate;    
-    // console.log('Выбранное время', selectedDates[0].getTime());
-    // console.log(diferentTime);    
-  },  
+    if (selectedDates[0].getTime() < Date.now()) {
+      window.alert('Please choose a date in the future');
+      selectedDates[0] = new Date();
+      refs.startBtn.setAttribute('disabled', true);
+      return;
+    } else {
+      refs.startBtn.removeAttribute('disabled');
+      selectedTime = selectedDates[0].getTime();
+      console.log(selectedTime);
+    }
+  },
 };
-console.log(options);
+
 const calendar = flatpickr('#datetime-picker', options);
 
-console.log("время для работы", calendar.selectedDates[0].getTime());
+const timer = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      const currentDate = Date.now();
+      // console.log("selectedTime", selectedTime);
+      const timerTime = selectedTime - currentDate;
+      const timeComponents = convertMs(timerTime);
+      apdateTimer(timeComponents);
+      // console.log("срабатывание текущее", currentDate);
+      // console.log(timerTime);
+      console.log(timeComponents);
+      if (timerTime <= 1000) {
+        console.log("НАЧАЛОСЬ!!!!!");
+        clearInterval(this.intervalId);
+        return;
+      };
+    }, 1000);
+  },
 
-// const timer = {
-//   start() {
-//     const futureDate = calendar.selectedDates[0];
-//     console.log(futureDate);
-//     setInterval(() => {
-//       const currentDate = Date.now();
-//       console.log("срабатывание текущее", currentDate);
-//       console.log('Выбранное', futureDate);
-//       console.log(futureDate-currentDate);
-//     },1000);
-//   },
-// };
+  
+};
 
-// timer.start();
+refs.startBtn.addEventListener('click', () => {
+  timer.start();
+});
 
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   // Remaining days
-//   const days = Math.floor(ms / day);
-//   // Remaining hours
-//   const hours = Math.floor((ms % day) / hour);
-//   // Remaining minutes
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-//   // Remaining seconds
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-//   return { days, hours, minutes, seconds };
-// }
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+function apdateTimer({days, hours, minutes, seconds}) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
